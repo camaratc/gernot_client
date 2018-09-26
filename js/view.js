43b73556f6bbaefc402d7ba4a3508e4a312f6722
Matheus <matheusrf96@gmail.com>
2018-09-26 14:25:53 -0300

    Implementação do intervalo de requisição

--- js/view.js
+++ js/view.js
@@ -1,8 +1,9 @@
 const notifier = require('node-notifier');
 const path = require('path');
 const http = require('http');
- 
 const axios = require('axios');
+
+const minutes = 0.25, theInterval = minutes * 60 * 1000;
 
 function categoria(tag){
     if(tag === 0) return "Erro";
@@ -11,9 +12,6 @@ function categoria(tag){
 }
 
 function searchNotification(){
-    let writeContent = document.getElementById('content');
-    writeContent.innerHTML = ``;
-
     axios.get('http://127.0.0.1:8000/api/notification/')
 
     .then(response => {
@@ -28,16 +26,6 @@ function searchNotification(){
             }, (err, response) => {
                 console.log(err);    
             });
-
-            writeContent.innerHTML += `
-                ID: ${response.data[i].pk} <br />
-                Título: ${item.title} <br />
-                Mensagem: ${item.message} <br />
-                Autor: ${item.author} <br />
-                Data de Envio: ${item.send_date} <br />
-                Categoria: ${categoria(item.tag)} <br />
-                <br />
-            `;
         }
     })
     .catch(error => {
@@ -45,15 +33,8 @@ function searchNotification(){
     });
 }
 
-/* document.getElementById('notify').onclick = (event) => {
-    notifier.notify ({
-        title: 'Teste Notificação',
-        message: 'Esta é uma notificação teste...',
-        icon: path.join('','/home/ayushgp/Desktop/images.png'),
-        sound: true,
-        wait: true            
-    }, (err, response) => {
-        // ...
-    });
-}
- */
+setInterval(() => {
+    console.log("Realizando checagem...");
+    document.getElementById('content').innerHTML += 'Realizando Checagem...<br />';
+    searchNotification();
+}, theInterval);
